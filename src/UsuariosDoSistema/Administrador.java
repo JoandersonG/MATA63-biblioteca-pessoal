025ae16b2516d779b5b63550ac47aa8c1602c5e2
Vanessa Machado <88630419+NessaMd@users.noreply.github.com>
2022-06-01 20:34:28 -0300

    Adicionando classes de usuários do sistema

---
+++ src/UsuariosDoSistema/Administrador.java
@@ -0,0 +1,75 @@
+
+package UsuariosDoSistema;
+
+
+public class Administrador extends UsuarioDoSistema {
+
+    public Administrador(String idUsuario, String nomeDeUusario, String userName, int senha) {
+        super(idUsuario, nomeDeUusario, userName, senha);
+    }
+
+    @Override
+    public void consultarLivroIsbn() {
+        
+    }
+
+    @Override
+    public void consultarLivroPeloNome() {
+        
+    }
+
+    @Override
+    public void consultarLivroPeloCodBarra() {
+        
+    }
+    
+    public void consultarUsuarioPeloNome() {
+        
+    }
+    
+    public void consultarUsuarioPeloId() {
+        
+    }
+    
+    public void cadastrarUsuario() {
+        
+    }
+    
+    public void atualizarUsuario() {
+        
+    }
+    
+    public void removerUsuario() {
+        
+    }
+    
+    public void cadastrarLivro() {
+        
+    }
+    
+    public void atualizarLivro() {
+        
+    }
+    
+    public void removerLivro() {
+        
+    }
+    
+    public void consultarReserva() {
+        
+    }
+    
+    public void consultarEmprestimo() {
+        
+    }
+    
+    public void definirRegrasDeEmprestimo() {
+        
+    }
+    
+    public void gerenciarEtiquetas() {
+        
+    }
+    
+    
+}
