705aeab43d9b2dac27138f149d9225b211d8513b
joanderson.goncalves <joanderson.goncalves@ufba.br>
2022-06-09 21:58:00 -0300

    refactor(VerificadorDeCampos): Minimize the amount of ifs in field verifier methods

--- src/main/java/com/ufba/eng/soft/bibliotecapessoal/util/VerificadorDeCampos.java
+++ src/main/java/com/ufba/eng/soft/bibliotecapessoal/util/VerificadorDeCampos.java
@@ -1,16 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.ufba.eng.soft.bibliotecapessoal.util;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-/**
- *
- * @author joand
- */
 public class VerificadorDeCampos {
     
     private VerificadorDeCampos () {
@@ -18,12 +7,8 @@ public class VerificadorDeCampos {
     }
     
     public static ResultadoVerificacao nome(String nome) {
-        if (nome == null) {
-            return new ResultadoVerificacao(false, "Campo Nome não  pode ser nulo.");
-        }
-        if (nome.isEmpty()){
-            return new ResultadoVerificacao(false, "Campo Nome não pode estar vazio.");
-        } else {
+        if (nome == null || nome.isEmpty()) {
+            return new ResultadoVerificacao(false, "Campo Nome não  pode estar vazio.");
         }
         if (!nome.matches("[a-zA-Z].+")) {
             return new ResultadoVerificacao(false, "Insira nome, Formato: Letras(maiúsculas e/ou minúsculas), sem caracteres especiais ou acentuação.");
@@ -32,12 +17,8 @@ public class VerificadorDeCampos {
     }
     
     public static ResultadoVerificacao sobrenome(String sobrenome) {
-        if (sobrenome == null) {
-            return new ResultadoVerificacao(false, "Campo Sobrenome não  pode ser nulo.");
-        }
-        if (sobrenome.isEmpty()){
+        if (sobrenome == null || sobrenome.isEmpty()) {
             return new ResultadoVerificacao(false, "Campo Sobrenome não pode estar vazio.");
-        } else {
         }
         if (!sobrenome.matches("[a-zA-Z].+")) {
             return new ResultadoVerificacao(false, "Insira Sobrenome, Formato: Letras(maiúsculas e/ou minúsculas), sem caracteres especiais ou acentuação.");
@@ -46,62 +27,32 @@ public class VerificadorDeCampos {
     }
     
     public static ResultadoVerificacao idUsuario(String idUsuario) {
-        if (idUsuario == null) {
-            return new ResultadoVerificacao(false, "Campo ID não  pode ser nulo.");
-        }
-        if (idUsuario .isEmpty()){
+        if (idUsuario == null || idUsuario .isEmpty()){
             return new ResultadoVerificacao(false, "Campo ID não  pode estar vazio.");
-        } else {
         }
-        if (idUsuario.length() == 6){
-           if (!idUsuario .matches("ID[0-9]+")) {
-                return new ResultadoVerificacao(false, "Insira o id do usuário, Formato: Inicial 'ID' seguidas de 4 dígitos, sem caracteres especiais, sem espaços.");
-            } 
+        if (idUsuario.length() != 6 || !idUsuario .matches("ID[0-9]+")){
+            return new ResultadoVerificacao(false, "Insira o id do usuário, Formato: Inicial 'ID' seguidas de 4 dígitos, sem caracteres especiais, sem espaços."); 
         }
-        else{
-            return new ResultadoVerificacao(false, "Insira o id do usuário, Formato: Inicial 'ID' seguidas de 4 dígitos, sem caracteres especiais, sem espaços.");
-        }
-        
         return new ResultadoVerificacao(true);
     }
     
     public static ResultadoVerificacao userName(String userName) {
-        if (userName == null) {
-            return new ResultadoVerificacao(false, "Campo Username não  pode ser nulo");
-        }
-        if (userName.isEmpty()){
+        if (userName == null || userName.isEmpty()){
             return new ResultadoVerificacao(false, "Campo Username não pode estar vazio");
-        } else {
         }
-        if(userName.length() >= 4 && userName.length() <= 10){
-            if (!userName.matches("[a-zA-Z]+")) {
-                return new ResultadoVerificacao(false, "Insira o Username, Formato: De 4 a 10 Letras (maiúsculas e/ou minúsculas), sem caracteres especiais ou acentuação, sem espaços.");
-            }
-        }
-        else {
+        if(userName.length() < 4 || userName.length() > 10 || !userName.matches("[a-zA-Z]+")){
             return new ResultadoVerificacao(false, "Insira o Username, Formato: De 4 a 10 Letras (maiúsculas e/ou minúsculas), sem caracteres especiais ou acentuação, sem espaços.");
         }
-        
         return new ResultadoVerificacao(true);
     }
     
     public static ResultadoVerificacao senha(String senha) {
-        if (senha == null) {
-            return new ResultadoVerificacao(false, "Campo senha não  pode ser nulo");
-        }
-        if (senha .isEmpty()){
+        if (senha == null || senha .isEmpty()){
             return new ResultadoVerificacao(false, "Campo senha não  pode estar vazio");
-        } else {
         }
-        if(senha.length() == 8){
-           if (!senha .matches("[0-9]+")) {
-                return new ResultadoVerificacao(false, "Insira a senha. Formato: 8 dígitos, sem espaços.");
-           } 
+        if(senha.length() != 8 || !senha .matches("[0-9]+")){
+           return new ResultadoVerificacao(false, "Insira a senha. Formato: 8 dígitos numéricos.");
         }
-        else{
-           return new ResultadoVerificacao(false, "Insira a senha. Formato: 8 dígitos, sem espaços."); 
-        }
-        
         return new ResultadoVerificacao(true);
     }
     
@@ -109,23 +60,15 @@ public class VerificadorDeCampos {
     
     //Verificação campos cadastro de livros
     public static ResultadoVerificacao titulo(String titulo) {
-        if (titulo == null) {
-            return new ResultadoVerificacao(false, "Campo título não pode ser nulo.");
-        }
-        if (titulo.isEmpty()){
+        if (titulo == null || titulo.isEmpty()){
             return new ResultadoVerificacao(false, "Campo título não pode estar vazio.");
-        } else {
         }
         return new ResultadoVerificacao(true);
     }
     
     public static ResultadoVerificacao autor(String autor) {
-        if (autor == null) {
-            return new ResultadoVerificacao(false, "Campo autor não  pode ser nulo.");
-        }
-        if (autor.isEmpty()){
+        if (autor == null || autor.isEmpty()){
             return new ResultadoVerificacao(false, "Campo autor não  pode estar vazio.");
-        } else {
         }
         if (!autor.matches("[a-zA-Z].+ [a-zA-Z].+")) {
             return new ResultadoVerificacao(false, "Insira nome e sobrenome do autor. Formato: Letras(maiúsculas e/ou minúsculas), sem caracteres especiais ou acentuação");
@@ -134,19 +77,10 @@ public class VerificadorDeCampos {
     }
     
     public static ResultadoVerificacao idAutor(String idAutor) {
-        if (idAutor == null) {
-            return new ResultadoVerificacao(false, "Campo ID-Autor(a) não  pode ser nulo.");
-        }
-        if (idAutor .isEmpty()){
+        if (idAutor == null || idAutor .isEmpty()) {
             return new ResultadoVerificacao(false, "Campo ID-Autor(a) não  pode estar vazio.");
-        } else {
-        }
-        if (idAutor.length() == 6){
-           if (!idAutor .matches("ID[0-9]+")) {
-                return new ResultadoVerificacao(false, "Insira o ID-Autor(a), Formato: Inicial 'ID' seguidas de 4 dígitos, sem caracteres especiais, sem espaços.");
-            } 
-        }
-        else{
+        }        
+        if (idAutor.length() != 6 || !idAutor .matches("ID[0-9]+")){
             return new ResultadoVerificacao(false, "Insira o ID-Autor(a), Formato: Inicial 'ID' seguidas de 4 dígitos, sem caracteres especiais, sem espaços.");
         }
         return new ResultadoVerificacao(true);
@@ -156,31 +90,20 @@ public class VerificadorDeCampos {
         if (isbn == null || isbn .isEmpty()) {
             return new ResultadoVerificacao(false, "Insira um válor numérico (4 a 13 dígitos) no campo ISBN.");
         }
-        if (!(isbn.length() >= 4) || !(isbn.length() <= 13) || !isbn .matches("[0-9]+")){
+        if (isbn.length() < 4 || isbn.length() > 13 || !isbn .matches("[0-9]+")){
                 return new ResultadoVerificacao(false, "Insira o ISBN, Formato: de 4 a 13 dígitos numéricos, sem caracteres especiais, sem espaços.");  
         }
         return new ResultadoVerificacao(true);
     }
     
     public static ResultadoVerificacao codDeBarra(String codDeBarra) {
-        if (codDeBarra == null) {
-            return new ResultadoVerificacao(false, "Campo Cód. De Barra não  pode ser nulo.");
+        if (codDeBarra == null || codDeBarra .isEmpty()) {
+            return new ResultadoVerificacao(false, "Campo Cód. De Barra não pode estar vazio.");
         }
-        if (codDeBarra .isEmpty()){
-            return new ResultadoVerificacao(false, "Campo Cód. De Barra não  pode estar vazio.");
-        } else {
-        }
-        if(codDeBarra.length() == 10){
-            if (!codDeBarra .matches("[0-9]+")) {
-                return new ResultadoVerificacao(false, "Insira o código de barra do livro, Formato: 10 dígitos, sem espaços.");
-            }
-        }
-        else {
+        if(codDeBarra.length() != 10 || !codDeBarra .matches("[0-9]+")){
             return new ResultadoVerificacao(false, "Insira o código de barra do livro, Formato: 10 dígitos, sem espaços.");
-        }
-        
+        }        
         return new ResultadoVerificacao(true);
     }
-    
     
 }
