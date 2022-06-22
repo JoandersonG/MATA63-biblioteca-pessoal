4bf4a9b029843fef7ea2c226479fd139161ebc17
taynavlv <88676777+taynavlv@users.noreply.github.com>
2022-06-22 17:21:53 -0300

    Update Livro.java

--- src/main/java/com/ufba/eng/soft/bibliotecapessoal/model/product/Livro.java
+++ src/main/java/com/ufba/eng/soft/bibliotecapessoal/model/product/Livro.java
@@ -18,13 +18,19 @@ public class Livro {
     public GeneroLivro genero;
     private boolean emprestado = false;
     private boolean reservado = false;
+    private ArrayList<Aluno> alunosReserva;
+    private ArrayList<Professor> professoresReserva;
+    private ArrayList<Orientando> orientandosReserva;
 
-        public Livro(String nomeDoLivro, String codigoISBN, String codigoDeBarras, Autor autor, GeneroLivro genero) {
+    public Livro(String nomeDoLivro, String codigoISBN, String codigoDeBarras, Autor autor, GeneroLivro genero) {
         this.nomeDoLivro = nomeDoLivro;
         this.codigoISBN = codigoISBN;
         this.codigoDeBarras = codigoDeBarras;
         this.autor = autor;
         this.genero = genero;
+        this.alunosReserva = new ArrayList<>();
+        this.professoresReserva = new ArrayList<>();
+        this.orientandosReserva = new ArrayList<>();
     }
     
     public boolean getReservado() {
@@ -33,6 +39,38 @@ public class Livro {
 
     public void setReservado(boolean reservado) {
         this.reservado = reservado;
+    }
+    
+    public String getCodigoISBN() {
+        return codigoISBN;
+    }
+
+    public void setCodigoISBN(String isbn) {
+        this.codigoISBN = isbn;
+    }
+    
+    public ArrayList<Aluno> getAlunosReserva() {
+        return alunosReserva;
+    }
+    
+    public ArrayList<Professor> getProfessoresReserva() {
+        return professoresReserva;
+    }
+    
+    public ArrayList<Orientando> getOrientandosReserva() {
+        return orientandosReserva;
+    }
+
+    public void setAlunosReserva(Aluno usuario) {
+        this.alunosReserva.add(usuario);
+    }
+    
+    public void setProfessoresReserva(Professor usuario) {
+        this.professoresReserva.add(usuario);
+    }
+    
+    public void setOrientandosReserva(Orientando usuario) {
+        this.orientandosReserva.add(usuario);
     }
     
 }
